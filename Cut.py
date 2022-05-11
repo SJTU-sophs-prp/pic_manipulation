17271c6a13e59f78691696cb50df02de5450d748
huangjiawei <huangjiawei@sjtu.edu.cn>
2022-05-11 11:46:10 +0800

    auto-resize

--- Cut.py
+++ Cut.py
@@ -30,6 +30,7 @@ def ver_cut(img_root, width, height, overlap_rate, save_root, start_point, zfill
 def total_cut(img_root, resize, width, height, overlap_x, overlap_y, save_root, start_point, zfill_num):
     im = Image.open(img_root)
     im = im.resize(resize)
+    print(resize)
     img_size = im.size
     m = img_size[0]  # 读取图片的宽度
     n = img_size[1]  # 读取图片的高度
@@ -50,24 +51,41 @@ def total_cut(img_root, resize, width, height, overlap_x, overlap_y, save_root, 
             y = start_loc_y * j  # 裁剪起点的y坐标范围
             region = im.crop((x, y, x + w, y + h))  # 裁剪区
             region.save(save_root + str(row * i + j + start_point).zfill(zfill_num) + ".jpg")  # str(i)是裁剪后的编号
+
+    print("end_point:",col*row+start_point)
     print("finish")
 
+def cal_resize(img_root, mul_rate, width, height, overlap_x, overlap_y):
+    im = Image.open(img_root)
+    xorigin = im.size[0]/mul_rate
+    yorigin = im.size[1]/mul_rate
+    start_loc_x = int(width * (1 - overlap_x))
+    start_loc_y = int(height * (1 - overlap_y))
+    x = math.ceil((xorigin-width)/(start_loc_x))*start_loc_x+width
+    y = math.ceil((yorigin-height)/(start_loc_y))*start_loc_y+height
+    resize = [x,y]
+    return resize
 
 if __name__ == "__main__":
-    img_root = "SSJ.jpg"  # 原始图片路径
+    img_root = "0506trainset/无用师4.jpg"  # 原始图片路径
     width = 256  # 设置你要裁剪的小图的宽度
     height = 256  # 设置你要裁剪的小图的高度
-    overlap_rate = 1 / 2  # 等于 overlap_rate_y
-    start_point = 2257  # 序号从第n张开始保存，默认为0；若要从第01234.jpg开始保存，则 start_point 为 1234
+    overlap_rate = 3 / 4  # 等于 overlap_rate_y
+    start_point = 28549  # 序号从第n张开始保存，默认为0；若要从第01234.jpg开始保存，则 start_point 为 1234
     zfill_num = 5  # 文件名长度，如 00011.jpg 的 zfill_num 为 5
-    save_root = "./SSJ/"
+    save_root = "./0511_multi_tr/"
 
     # 单条裁剪
     # ver_cut(img_root, width, height, overlap_rate, save_root, start_point, zfill_num)
 
     # For Wuyong & Shengshan
-    resize = (4096, 2560)  # For multiscaling
+    # resize = (4096, 2560)  # For multiscaling
+    mul_rate =  2           # For multiscaling 即裁出来的256图包含最开始256图信息的比例
     overlap_x = 3/4
     overlap_y = 3/4
 
-    total_cut(img_root, resize, width, height, overlap_x, overlap_y, save_root, start_point, zfill_num)
+    print("img_root:",img_root)
+    print("start_point:",start_point)
+    print("mul_rate:",mul_rate)
+    resize = cal_resize(img_root, mul_rate, width, height, overlap_x, overlap_y)
+    total_cut(img_root, resize,width, height, overlap_x, overlap_y, save_root, start_point, zfill_num)
